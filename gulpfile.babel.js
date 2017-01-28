d29fd416849b57a052c32ae1aea1722fea48a02a
Owen Buckley <owen@analogstudios.net>
2017-01-27 20:01:47 -0500

    move to NPM scripts and sample src file

--- gulpfile.babel.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp';
-import validateJsPipeline from 'pipeline-validate-js';
-
-gulp.task('lint', () => {
-  gulp.src('./**/**/.js')
-    .pipe(validateJsPipeline.validateJS());
-});
-
-gulp.task('build', ['lint']);
