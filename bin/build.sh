06afbb196cd4fa57e0697240b829c141c3f2a9fc
Owen Buckley <obuckley@kenzan.com>
2017-01-27 20:14:51 -0500

    initial commit of repo files and build task (#8)
    * initial commit
    
    * add build task to build script
    
    * move to NPM scripts and sample src file

---
+++ bin/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "environment information"
+node -v
+npm -v
+
+echo "installing dependencies"
+rm -rf node_modules > /dev/null 2>&1
+npm cache clean
+npm install
+
+echo "running the build"
+npm run build
---
+++ src/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const OFFSET = 4;
+
+function myFunction (offset) {
+  // console.log('test');
+  let x = 2;
+
+  return x + offset;
+}
+
+myFunction(OFFSET);
